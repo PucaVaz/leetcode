75b22f96740a0710cb02ef6db108f5ad6d136ff9
Puca <pucantc@gmail.com>
2024-08-03 09:47:24 -0300

    Time: 465 ms (61.30%) | Memory: 30.8 MB (55.81%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -4,16 +4,8 @@ class Solution:
 
         for i, n  in enumerate(nums):
             if(nums[i] != 0):
-                if arr[n] > 1:
+                if arr[n] >= 1:
                     return n
                 arr[n] += 1 
-
-        i = 1
-        while i < len(nums) + 1:
-            if arr[i] > 1:
-                return i         
-            i += 1
-
-        return 0
 
         return 0
